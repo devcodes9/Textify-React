cda9383d31f440478bb7b0147b9c3a59dbe87a42
Dev Samir Dalia <devdalia9@gmail.com>
2022-12-26 22:20:50 +0530

    Adding text State

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const TextForm = (props) => {
+  const [text, setText] = useState("Enter text here")
+  
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  }
+  const handleUpper = () => {
+    let newtext = text.toUpperCase();
+    setText(newtext);
+  }
   return (
     <div>
         <h1>{props.heading}</h1>
         <div className="form-group">
-        <textarea className="form-control" id="myBox" rows="8"></textarea>
+        <textarea className="form-control" id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
         </div>
-        <button className="btn btn-primary my-3">Uppercase</button>
+        <button className="btn btn-primary my-3" onClick={handleUpper}>Uppercase</button>
     </div>
   )
 }
